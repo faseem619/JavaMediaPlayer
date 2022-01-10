92f1b68a887a90e6e09e9156cf8d0076da05d7f3
faseem619 <faseem619@gmail.com>
2022-01-10 16:05:51 +0530

    added playback functinoality

--- src/Footer.java
+++ src/Footer.java
@@ -1,14 +1,90 @@
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
+import javax.swing.JLabel;
 import javax.swing.JPanel;
+import javax.swing.event.MouseInputAdapter;
+
+import java.awt.event.MouseEvent;
+
 import java.awt.FlowLayout;
 import java.awt.Color;
 import java.awt.Dimension;
+import java.io.File;
+import java.io.IOException;
+import java.awt.image.BufferedImage;
+import java.awt.Image;
+import javax.sound.sampled.*;
+
+
 
 public class Footer extends JPanel {
+    JLabel playPausebtn;
+    Boolean playing = false;
+    Clip clip;
     Footer(){
         this.setLayout(new FlowLayout());
-        this.setPreferredSize(new Dimension(400,50));
-        this.setBackground(Color.red);
+        this.setPreferredSize(new Dimension(400,120));
+        this.setBackground(new Color(0x8843F2));
 
+        // buttons
+
+        playPausebtn = new JLabel();
+        playPausebtn.setSize(new Dimension(70,70));;
+        setAsIcon(playPausebtn, "play-button.png");
+        playPausebtn.addMouseListener(new MouseInputAdapter() {
+           
+            public void mouseClicked(MouseEvent me){
+                if(playing){
+                    setAsIcon(playPausebtn, "play-button.png");
+                    playing = false;
+                    clip.stop();
+                }
+                else if(clip!=null){
+                    setAsIcon(playPausebtn,"Pause-Button.png");
+                    playing =true;setAsIcon(playPausebtn,"Pause-Button.png");
+                    playing =true;
+
+                    clip.start();
+
+                }
+
+
+            }
+        });
+        this.add(playPausebtn);
+
+
+
+    }
+    public void setAsIcon(JLabel label,String filepath){
+    BufferedImage img = null;
+    try {
+        img = ImageIO.read(new File(filepath));
+    } catch (IOException e) {
+        e.printStackTrace();
+    }
+    finally{
+
+        Image dimg = img.getScaledInstance(label.getWidth(), label.getHeight(),
+            Image.SCALE_SMOOTH);
+        label.setIcon(new ImageIcon(dimg));
+    }
+
+    }
+
+    public void setClip(AudioInputStream audioStream){
+        System.out.println("clip test");
+        try {
+            clip = AudioSystem.getClip();
+            clip.open(audioStream);
+        } catch (LineUnavailableException e) {
+
+            e.printStackTrace();
+        } catch (IOException e) {
+
+            e.printStackTrace();
+        }
+        
     }
 
     
--- src/Player.java
+++ src/Player.java
@@ -1,19 +1,72 @@
 import java.awt.BorderLayout;
 
+import javax.swing.JFileChooser;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
+import javax.swing.JMenu;
+import javax.swing.JMenuBar;
+import javax.swing.JMenuItem;
+import javax.swing.filechooser.FileNameExtensionFilter;
 
-public class Player {
+import java.awt.event.*;
+import java.io.File;
+import java.io.IOException;
+
+
+import javax.sound.sampled.*;
+
+public class Player implements ActionListener{
+    JMenu fileMenu;
+    JMenuItem openItem;
+    Footer footer;
     Player(){
         JFrame frame = new JFrame("Media Player");
         frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
         frame.setLocationRelativeTo(null);
         frame.setSize(400,400);
+
+        // Menubar
+        JMenuBar menuBar = new JMenuBar();
+
+        fileMenu = new JMenu("File");
+        openItem = new JMenuItem("Open");
+        openItem.addActionListener(this);
+        fileMenu.add(openItem);
         
+        menuBar.add(fileMenu);
+
         frame.add(new JLabel("Test"));
-        frame.add(new Footer(),BorderLayout.SOUTH);
+        footer = new Footer();
+        frame.add(footer,BorderLayout.SOUTH);
+        frame.setJMenuBar(menuBar);
         frame.setVisible(true);
 
     }
+    @Override
+    public void actionPerformed(ActionEvent e) {
+        if(e.getSource()==openItem){
+            JFileChooser fileChooser = new JFileChooser();
+           fileChooser.setCurrentDirectory(new File("."));
+           fileChooser.setFileFilter( new FileNameExtensionFilter("Wav Files", "wav"));
+
+           int response = fileChooser.showOpenDialog(null);
+           if(response==JFileChooser.APPROVE_OPTION){
+               File myFile = new File(fileChooser.getSelectedFile().getAbsolutePath());
+               try {
+                AudioInputStream audioStream = AudioSystem.getAudioInputStream(myFile);
+                
+                footer.setClip(audioStream);
+            } catch (UnsupportedAudioFileException e1) {
+
+                e1.printStackTrace();
+            } catch (IOException e1) {
+
+                e1.printStackTrace();
+            } 
+                
+            }
+        
+        }
     
+    }
 }
