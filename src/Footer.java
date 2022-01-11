013a5f0ed48b3e983c248e487fbb221f4d93b3ec
faseem619 <faseem619@gmail.com>
2022-01-11 12:33:04 +0530

    added volume controls

--- src/Footer.java
+++ src/Footer.java
@@ -3,6 +3,8 @@ import javax.swing.ImageIcon;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
 import javax.swing.JSlider;
+import javax.swing.event.ChangeEvent;
+import javax.swing.event.ChangeListener;
 import javax.swing.event.MouseInputAdapter;
 
 
@@ -10,6 +12,7 @@ import javax.swing.event.MouseInputAdapter;
 import java.awt.event.MouseEvent;
 
 import java.awt.FlowLayout;
+import java.awt.BorderLayout;
 import java.awt.Color;
 import java.awt.Dimension;
 import java.io.File;
@@ -25,13 +28,24 @@ public class Footer extends JPanel {
     JLabel forwardBtn;
     JLabel backBtn;
     JSlider volumeSlider;
+    JLabel volumeBtn;
     Boolean playing = false;
+    Boolean muted =false;
+    int prevVolume;
+    FloatControl volCtrl;
     Clip clip;
     Footer(){
         // -------Panel Properties----
-        this.setLayout(new FlowLayout());
         this.setPreferredSize(new Dimension(400,120));
         this.setBackground(new Color(0x8843F2));
+        this.setLayout(new BorderLayout());
+
+        JPanel topPanel = new JPanel();
+        topPanel.setBackground(new Color(0x8843F2));
+
+        JPanel bottomPanel = new JPanel();
+        bottomPanel.setBackground(new Color(0x8843F2));
+        bottomPanel.setLayout(new FlowLayout(FlowLayout.LEADING));
 
         // -----Buttons -----------
 
@@ -52,6 +66,7 @@ public class Footer extends JPanel {
                     playing =true;
 
                     clip.start();
+                    clip.loop(100);
 
 
                 }
@@ -84,15 +99,58 @@ public class Footer extends JPanel {
         });
 
         // ---- Volume Control--------
-        volumeSlider = new JSlider();
+        volumeSlider = new JSlider(-35,0);
         volumeSlider.setSize(new Dimension(100,10));
-        volumeSlider.setLocation(40, 60);
+        volumeSlider.setValue(-5);
+        volumeSlider.addChangeListener(new ChangeListener() {
+            @Override
+            public void stateChanged(ChangeEvent e){
+                JSlider source = (JSlider)e.getSource();
+                if (!source.getValueIsAdjusting()) {
+                    int volume = source.getValue();
+                    volume = volume<-34?-80:volume;
+                    volCtrl.setValue(volume);
+                    
+                
+            }
+            }
+
+            });
+
+        volumeBtn = new JLabel();
+        volumeBtn.setSize(30,30);
+        volumeBtn.addMouseListener(new MouseInputAdapter() {
+            public void mouseClicked(MouseEvent me){
+                if(muted){
+                    volCtrl.setValue(prevVolume>-34?prevVolume:-80);
+                    volumeSlider.setValue(prevVolume);
+                    setAsIcon(volumeBtn, "volume.png");
+                    muted=false;
+                }
+                else{
+                    volCtrl.setValue(-80);
+                    prevVolume = volumeSlider.getValue();
+                    volumeSlider.setValue(-35);
+                    setAsIcon(volumeBtn, "mute.png");
+                    muted = true;
+
+                }
+
+            }
+        });
+        setAsIcon(volumeBtn, "volume.png");
         
 
 
-        this.add(backBtn);
-        this.add(playPausebtn);
-        this.add(forwardBtn);
+        topPanel.add(backBtn);
+        topPanel.add(playPausebtn);
+        topPanel.add(forwardBtn);
+
+        bottomPanel.add(volumeBtn);
+        bottomPanel.add(volumeSlider);
+        
+        this.add(topPanel,BorderLayout.NORTH);
+        this.add(bottomPanel,BorderLayout.SOUTH);
 
 
 
@@ -125,6 +183,9 @@ public class Footer extends JPanel {
 
             e.printStackTrace();
         }
+        volCtrl = (FloatControl)clip.getControl(FloatControl.Type.MASTER_GAIN);
+        volCtrl.setValue(-5);
+        
         
     }
 
